74c54f90a9ebc8cbf4bdbfd83db327ee4a30ada9
ilia5 <50524579+ilia5@users.noreply.github.com>
2020-07-11 15:19:28 +0200

    Add files via upload

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <h1>About</h1>
-    <p>Text here....</p>
+    <h2>About the Study Program Organization Assistant:</h2>
+    <h3>
+    <p style="padding-left:4em">
+    The "Study Program Organization Assistant" is a tool that helps you plan and organize your upcoming semester
+    by giving you the necessary information to choose which courses are best for you.
+    </p>
+    <br>
+    </h3>
+    <h2>About us:</h2>
+    <h3>
+      We are:
+      <p style="padding-left:4em">
+         Ilia Nassif (M.Sc. Computer Engineering(Interactive Systems and Visualization) Student)
+      <br>
+      Deniz Glawenda (B.Sc. Angewandte Kognitions- und Medienwissenschaft student)
+      </p>
+       We created this tool as a part of the LAVA lab uner the guidance of:
+      <p style="padding-left:4em"> 
+          Prof. Dr. Mohamed Amine Chatti   and
+          Dr. Arham Muslim
+      </p>
+      </h3>
+      <br>
+      <h2>About LAVA:</h2>
+      <h3>
+      <p style="padding-left:4em"> 
+      The LAVA lab (Learning Analytics and Visual Analytics) is a project that teaches students
+      how to design and implement interactive visualizations following the Human Centered Design process
+      and the What-Why-How visualization analysis framework.
+      </p>
+      </h3>
   </div>
 </template>
